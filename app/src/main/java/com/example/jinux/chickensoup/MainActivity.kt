f026cdd32582ac8c4c56eed724c6afb08251cb63
jinux <jinguol999@163.com>
2017-06-27 22:59:21 +0800

    refactor: seperate the logic from ui

--- app/src/main/java/com/example/jinux/chickensoup/MainActivity.kt
+++ app/src/main/java/com/example/jinux/chickensoup/MainActivity.kt
@@ -12,22 +12,29 @@ import android.widget.Spinner
 import android.widget.TextView
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
-import org.jetbrains.anko.sdk25.coroutines.onFocusChange
+import org.jetbrains.anko.sdk25.coroutines.onItemSelectedListener
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val ui = MainActivityUI()
         ui.setContentView(this)
+
+        val mMainPresenter: MainPresenter = MainPresenter(this)
+        mMainPresenter.attachView(ui)
+        ui.setPresenter(mMainPresenter)
     }
 }
 
 class MainActivityUI : AnkoComponent<MainActivity> {
-    lateinit var sumScore: TextView
-    lateinit var base: TextView
-    lateinit var newScore: TextView
-    lateinit var actionSpinner: Spinner
+
+    lateinit var mMainPresenter: MainPresenter
+
+    lateinit private var mSumScoreTv: TextView
+
+    lateinit private var mActionSpin: Spinner
 
     override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
         relativeLayout {
@@ -36,11 +43,14 @@ class MainActivityUI : AnkoComponent<MainActivity> {
 
             val scoreLine = linearLayout {
                 id = android.R.id.text1
-                actionSpinner = spinner {
+                mActionSpin = spinner {
                     adapter = ArrayAdapter<String>(ui.ctx, android.R.layout.simple_list_item_1,
                             resources.getStringArray(R.array.action))
+                    onItemSelectedListener {
+                        toast("Hello" + selectedItem)
+                    }
                 }
-                sumScore = textView {
+                mSumScoreTv = textView {
                     hint = "总成绩"
                     text = "0"
                     gravity = Gravity.CENTER
@@ -48,7 +58,7 @@ class MainActivityUI : AnkoComponent<MainActivity> {
                 textView {
                     text = " = "
                 }
-                base = editText {
+                editText {
                     hint = "基数"
                     gravity = Gravity.CENTER
                     inputType = InputType.TYPE_CLASS_NUMBER
@@ -56,11 +66,11 @@ class MainActivityUI : AnkoComponent<MainActivity> {
                     setSelectAllOnFocus(true)
                     addTextChangedListener(object : TextWatcher {
                         override fun afterTextChanged(s: Editable?) {
-                            if (s == null || s.isEmpty()) return
+                            if (s == null || s.isEmpty()) {
+                                return
+                            }
 
-                            val base = base.text.toString().toInt()
-                            val new = newScore.text.toString().toInt()
-                            sumScore.text = (base + new).toString()
+                            mMainPresenter.onBaseEditChanged(s.toString().toInt())
                         }
 
                         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -69,11 +79,12 @@ class MainActivityUI : AnkoComponent<MainActivity> {
                         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                         }
 
-                    })                }
+                    })
+                }
                 textView {
                     text = " + "
                 }
-                newScore = editText {
+                editText {
                     hint = "新成绩"
                     gravity = Gravity.CENTER
                     setText(context.getString(R.string.edit_default_value))
@@ -81,11 +92,11 @@ class MainActivityUI : AnkoComponent<MainActivity> {
                     inputType = InputType.TYPE_CLASS_NUMBER
                     addTextChangedListener(object : TextWatcher {
                         override fun afterTextChanged(s: Editable?) {
-                            if (s == null || s.isEmpty()) return
+                            if (s == null || s.isEmpty()) {
+                                return
+                            }
 
-                            val base = base.text.toString().toInt()
-                            val new = newScore.text.toString().toInt()
-                            sumScore.text = (base + new).toString()
+                            mMainPresenter.onNewScoreEditChanged(s.toString().toInt())
                         }
 
                         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -103,16 +114,7 @@ class MainActivityUI : AnkoComponent<MainActivity> {
             button {
                 text = "分享"
                 onClick {
-                    val sum = sumScore.text.toString().toInt()
-                    val base = base.text.toString().toInt()
-                    val new = newScore.text.toString().toInt()
-                    val action = actionSpinner.selectedItem
-
-                    val msg = "$action: $sum = $base + $new"
-                    val intent = Intent(Intent.ACTION_SEND)
-                    intent.putExtra(Intent.EXTRA_TEXT, msg)
-                    intent.setType("text/plain")
-                    ui.ctx.startActivity(intent)
+                    mMainPresenter.onShareClick()
                 }
             }.lparams {
                 below(scoreLine)
@@ -121,5 +123,55 @@ class MainActivityUI : AnkoComponent<MainActivity> {
             }
         }
     }.view()
+
+    fun updateSumScore(sum: Int) {
+        mSumScoreTv.text = sum.toString()
+    }
+
+    fun setPresenter(mainPresenter: MainPresenter) {
+        mMainPresenter = mainPresenter
+    }
+
+    fun getAction(): String {
+        return mActionSpin.selectedItem.toString()
+    }
+}
+
+class MainPresenter(val mContext: MainActivity) {
+    private var mBaseScore: Int = 0
+    private var mNewScore: Int = 0
+    private var mSumScore: Int = 0
+
+    fun onBaseEditChanged(baseScore: Int) {
+        mBaseScore = baseScore
+        updateSumScore()
+    }
+
+    private fun updateSumScore() {
+        mSumScore = mBaseScore + mNewScore
+        mView.updateSumScore(mSumScore)
+    }
+
+
+    fun onNewScoreEditChanged(newScore: Int) {
+        mNewScore = newScore
+        updateSumScore()
+    }
+
+    fun onShareClick() {
+        val action = mView.getAction()
+
+        val msg = "$action: $mSumScore = $mBaseScore + $mNewScore"
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_TEXT, msg)
+        intent.type = "text/plain"
+        mContext.ctx.startActivity(intent)
+    }
+
+    lateinit private var mView: MainActivityUI
+
+    fun attachView(view: MainActivityUI) {
+        mView = view
+    }
 
 }
