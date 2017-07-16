d0b62dd4b246def7c91f0f680fd56e389da17113
jinux <jinguol999@163.com>
2017-07-16 22:40:12 +0800

    change: reverse the sort of records list

--- app/src/main/java/com/example/jinux/chickensoup/main/MainActivityUI.kt
+++ app/src/main/java/com/example/jinux/chickensoup/main/MainActivityUI.kt
@@ -286,7 +286,7 @@ class MainActivityUI : AnkoComponent<MainActivity> {
     }
 
     fun updateRecords(data: List<RecordItem>) {
-        mRecords.adapter = RecordListAdapter(data)
+        mRecords.adapter = RecordListAdapter(data.reversed())
     }
 
     fun showWarnNewScoreTooSmall() {
